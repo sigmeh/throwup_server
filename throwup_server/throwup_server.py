8d40cf24ef231339e919641d8b875a49a586f7f0
Mark Harvey <markedwinharvey@gmail.com>
2017-06-14 20:34:15 +0200

    add pkgutil for script access from imported module; streamline IO; improve documentation

--- throwup_server/throwup_server.py
+++ throwup_server/throwup_server.py
@@ -1,75 +1,37 @@
 #!/usr/bin/env python
 '''
-Start a python server in a remote directory (supplied as argument)
-
-Copy these files to target directory:
-	start_throwup.py	# invoke server script
-	serve_throwup.py	# actual server
-	throwup_filelist	# script for retrieving remote directory's contents
-						# 	accessed from http://127.0.0.1:port/throwup_filelist
-
+Start a python server in a remote directory on local machine
 
 Usage:
 	>>> from throwup_server import throwup_server
-	>>> throwup_server.throwup( path-to-directory [, port=port])
+	>>> throwup_server.throwup( path_to_dir=path_to_dir [, port=port])
 
-	$ python throwup_server.py [path_to_directory]
-	
+	$ python throwup_server.py [path_to_directory] [port]
+
+1. Kill any running process with the name 'serve_throwup.py'
+2. Copy server scripts to remote directory
+3. Start server from remote directory
+4. Test whether server is operational
+
+These files are copied to the target directory:
+	start_throwup.py	# invoke server script
+	serve_throwup.py	# actual server
+	filelist_throwup	# script for retrieving remote directory's contents
+						# 	accessed via http://127.0.0.1:port/filelist_throwup
+
 '''
+
 import sys
 import time
 import subprocess as sp
+import requests
+import pkgutil
 
-def throwup( **kwargs ):
-	
-	path_to_dir = kwargs.get('path_to_dir')
-	
-	if not path_to_dir:
-		print 'Need directory'
-		return 'Need directory'
-	
-		
-	#------------------
-	#------------------
-	# Read in file data 
-	#------------------
-	with open('serve_throwup.py','r') as f:
-		serve_throwup = f.read()
-		
-	with open('start_throwup.py','r') as f:
-		start_throwup = f.read().replace( '{{path_to_directory}}',path_to_dir )
-	
-	with open('throwup_filelist','r') as f:
-		throwup_filelist = f.read()
-	
-	
-	#------------------
-	# Write file data
-	#------------------
-	serve_path = path_to_dir+'/serve_throwup.py'
-	with open(serve_path,'w') as f:
-		f.write(serve_throwup)
-		
-	start_path = path_to_dir+'/start_throwup.py'	
-	with open(start_path,'w') as f:
-		f.write(start_throwup)
-	
-	throwup_path = path_to_dir+'/throwup_filelist'
-	with open(throwup_path,'w') as f:
-		f.write(throwup_filelist)
-	#------------------
-	#------------------
-
-
-	time.sleep(0.5)
-
-	cmd = 'cd %s; chmod +x *throwup*; python start_throwup.py' % path_to_dir
-	#cmd = 'cd %s; chmod +x *throwup*; python start_throwup.py port=%s' % ( path_to_dir, kwargs.get('port') )
-	sp.Popen(cmd,shell=True)
-	
-
+#
+#
+#
 def cleanup( **kwargs ):
-	'''Find and kill any serve_throwup.py'''
+	'''Find and kill any serve_throwup.py process'''
 	
 	path_to_dir = kwargs['path_to_dir']
 	
@@ -84,26 +46,118 @@ def cleanup( **kwargs ):
 			cmd = 'kill %s' %pid
 			sp.Popen(cmd.split())
 			print 'Done.'
+			
+#		
+#	
+#
+def test_server( path_to_dir, port ):
+	''' Test that new server is running using requests module '''
+	time.sleep(1)
 	
+	try:
+		r = requests.get('http://127.0.0.1:%s/' % port )
+		print 'Received status code from server :',r.status_code
+		print 'Server looks operational.'
+	except Exception as e:
+		print 'Error raised on server call with the following information:'
+		raise e
+		
+#
+#
+#
+def throwup( **kwargs ):
+	'''Attempt to start server in remote directory via the following:
+		1. run cleanup() to kill any server process currently running that is named serve_throwup.py
+		2. read the following files and write their contents to the remote directory at path_to_dir:
+			start_throwup.py	# script to start server
+			serve_throwup.py	# server script
+			filelist_throwup.py	# script to retrieve list of files in remote directory
+		3. use requests module to test for active server at 127.0.0.1:port where port is kwarg or 8002 by default
+	'''
 	
+	path_to_dir = kwargs.get('path_to_dir')
+	port = kwargs.get('port') if kwargs.get('port') else '8002'
 	
-def main():
-	''' Ensure filepath is included in function call '''
-	if len(sys.argv) == 1:
+	if not path_to_dir:
 		print 'Need directory'
 		return 'Need directory'
 	
-	path_to_dir = sys.argv[1]
 	
-	''' Kill any server processes named serve_throwup.py'''
+	#	
+	#------------------
+	# Kill any server processes named serve_throwup.py
+	#------------------
+	#
 	cleanup( path_to_dir=path_to_dir )
 	
-	''' Start new server in target directory'''
-	throwup( path_to_dir=path_to_dir )
+	
+	#	
+	#------------------
+	# Read in file data via pkgutil (for copying to remote directory)
+	#------------------
+	#	
+	script_names = ['start_throwup.py', 'serve_throwup.py', 'filelist_throwup']
+	
+	try: 
+		script_files = { x : pkgutil.get_data( 'throwup_server', x ) for x in script_names }
 		
-		
+	except Exception as e:
+		print 'Cannot locate script files for copying'
+		print e
+		sys.exit()	
+	
+	
+	#	
+	#------------------
+	# Write file data to remote directory
+	#------------------
+	#			
+	for script in script_files:
+		with open( '%s/%s' % (path_to_dir, script) , 'w' ) as f:
+			f.write( script_files[script] )
+
+	time.sleep(0.5)
+
+
+	#	
+	#------------------
+	# Start server via call to remote-directory/start_throwup.py 
+	#------------------
+	#
+	cmd = 'cd %s; chmod +x *throwup*; python start_throwup.py %s' % (path_to_dir, port)
+	#cmd = 'cd %s; chmod +x *throwup*; python start_throwup.py port=%s' % ( path_to_dir, kwargs.get('port') )
+	sp.Popen(cmd,shell=True)
+
+	
+	#	
+	#------------------
+	# Test that new server is running
+	#------------------
+	#
+	
+	test_server( path_to_dir=path_to_dir, port=port )
 	
 
-	pass
+
+def main():
+	''' Retrieve path_to_dir argument and port number if supplied '''
+	
+	if len(sys.argv) == 1:
+		print 'Need directory'
+		return 'Need directory'	
+	
+		
+	path_to_dir = sys.argv[1]
+	
+	port = sys.argv[2] if len(sys.argv) == 3 else 8002
+
+	#
+	#
+	''' Start new server in target directory'''
+	throwup( path_to_dir=path_to_dir, port=port )
+	#
+	#	
+
 if __name__ == '__main__':
 	main()
+	
